bdff6f224cd0542e5e6258906d1a67b3bd391a12
Pedro Clerici <pedro.clerici@tutamail.com>
2022-06-28 19:32:22 -0300

    Finished 3.4_readonly_property

---
+++ 3.0_classes_&_interfaces/3.4_readonly_property/src/app.ts
@@ -0,0 +1,36 @@
+// Read-only property:
+class Department {
+  // public name: string;
+  // private readonly id: number;
+  private employees: string[] = [];
+
+  constructor(public name: string, private readonly id: number) {
+    this.name = name;
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployees(this: Department, employee: string)  {
+    this.employees.push(employee);
+    // this.id++;
+  }
+
+  logEmployees(this: Department) {
+    for (let employee of this.employees) {
+      console.log(employee);
+    }
+  }
+}
+
+const accounting = new Department('Accounting', 1);
+console.log(accounting);
+
+accounting.addEmployees('Steve');
+accounting.addEmployees('Scott');
+accounting.addEmployees('Maria');
+// accounting.employees.push('Ana');
+
+accounting.logEmployees();
+accounting.describe();
