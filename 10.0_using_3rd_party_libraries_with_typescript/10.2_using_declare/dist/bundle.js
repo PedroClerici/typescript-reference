27aaf2035f6a0987fe74542961c791249001e9c2
Pedro Clerici <pedro.clerici@tutamail.com>
2022-07-15 23:50:52 -0300

    Finished 10.2_using_declare

---
+++ 10.0_using_3rd_party_libraries_with_typescript/10.2_using_declare/dist/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log(GLOBAL_VAR),console.log(GLOBAL_FUNCTION(10,12))})();
---
+++ 10.0_using_3rd_party_libraries_with_typescript/10.2_using_declare/src/app.ts
@@ -0,0 +1,5 @@
+declare var GLOBAL_VAR: string;
+declare function GLOBAL_FUNCTION(num1: number, num2: number): number;
+
+console.log(GLOBAL_VAR);
+console.log(GLOBAL_FUNCTION(10, 12));
